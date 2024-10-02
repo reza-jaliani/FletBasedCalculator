98f356b31a77373b67d0469039ebed00b64cb139
reza-jaliani <mrz.jaliani@gmail.com>
2024-10-02 06:11:22 +0330

    Design Calculator

--- calculator/main.py
+++ calculator/main.py
@@ -2,7 +2,40 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+    btn1 = ft.ElevatedButton(text="1")
+    btn2 = ft.ElevatedButton(text="2")
+    btn3 = ft.ElevatedButton(text="3")
+    btn4 = ft.ElevatedButton(text="4")
+    btn5 = ft.ElevatedButton(text="5")
+    btn6 = ft.ElevatedButton(text="6")
+    btn7 = ft.ElevatedButton(text="7")
+    btn8 = ft.ElevatedButton(text="8")
+    btn9 = ft.ElevatedButton(text="9")
+    btn0 = ft.ElevatedButton(text="0")
+    btnPlus = ft.ElevatedButton(text="+")
+    btnMinus = ft.ElevatedButton(text="-")
+    btnMultiply = ft.ElevatedButton(text="*")
+    btnDot = ft.ElevatedButton(text=".")
+    btnEqual = ft.ElevatedButton(text="=")
+    btnDivide = ft.ElevatedButton(text="/")
+    
+    page.add(ft.SafeArea(ft.Text("Hello, Flet!", color="Red")))
+    page.add(
+        ft.Row([btn7, btn8, btn9, btnPlus])
+    )
+    page.add(
+        ft.Row([btn4, btn5, btn6, btnMinus])
+    )
+    page.add(
+        ft.Row([btn1, btn2, btn3, btnMultiply])
+    )
+    page.add(
+        ft.Row([btn0, btnDot, btnDivide, btnEqual])
+    )
+    page.add(
+        ft.Row(controls=[
 
+        ])
+    )
 
 ft.app(main)
