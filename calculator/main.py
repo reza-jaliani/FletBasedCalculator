36de1b852742981f45547bef403e112b38b794ac
root <mrz.jaliani@gmail.com>
2024-10-03 12:29:52 +0330

    Defina ction for numeral buttons

--- calculator/main.py
+++ calculator/main.py
@@ -2,16 +2,25 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    btn1 = ft.ElevatedButton(text="1")
-    btn2 = ft.ElevatedButton(text="2")
-    btn3 = ft.ElevatedButton(text="3")
-    btn4 = ft.ElevatedButton(text="4")
-    btn5 = ft.ElevatedButton(text="5")
-    btn6 = ft.ElevatedButton(text="6")
-    btn7 = ft.ElevatedButton(text="7")
-    btn8 = ft.ElevatedButton(text="8")
-    btn9 = ft.ElevatedButton(text="9")
-    btn0 = ft.ElevatedButton(text="0")
+    num = ""
+    def btnNum_clicked(e):
+        nonlocal num
+        num += str(e.control.data)
+        t.value = num
+        page.update()
+    
+    t = ft.Text("Hello Flet!", color="Red")
+    btn0 = ft.ElevatedButton(text="0", on_click=lambda e:btnNum_clicked(e), data=0)
+    btn1 = ft.ElevatedButton(text="1", on_click=lambda e:btnNum_clicked(e), data=1)
+    btn2 = ft.ElevatedButton(text="2", on_click=lambda e:btnNum_clicked(e), data=2)
+    btn3 = ft.ElevatedButton(text="3", on_click=lambda e:btnNum_clicked(e), data=3)
+    btn4 = ft.ElevatedButton(text="4", on_click=lambda e:btnNum_clicked(e), data=4)
+    btn5 = ft.ElevatedButton(text="5", on_click=lambda e:btnNum_clicked(e), data=5)
+    btn6 = ft.ElevatedButton(text="6", on_click=lambda e:btnNum_clicked(e), data=6)
+    btn7 = ft.ElevatedButton(text="7", on_click=lambda e:btnNum_clicked(e), data=7)
+    btn8 = ft.ElevatedButton(text="8", on_click=lambda e:btnNum_clicked(e), data=8)
+    btn9 = ft.ElevatedButton(text="9", on_click=lambda e:btnNum_clicked(e), data=9)
+    
     btnPlus = ft.ElevatedButton(text="+")
     btnMinus = ft.ElevatedButton(text="-")
     btnMultiply = ft.ElevatedButton(text="*")
@@ -19,23 +28,19 @@ def main(page: ft.Page):
     btnEqual = ft.ElevatedButton(text="=")
     btnDivide = ft.ElevatedButton(text="/")
     
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!", color="Red")))
-    page.add(
+    
+    page.controls.append(ft.SafeArea(t))
+    page.controls.append(
         ft.Row([btn7, btn8, btn9, btnPlus])
     )
-    page.add(
+    page.controls.append(
         ft.Row([btn4, btn5, btn6, btnMinus])
     )
-    page.add(
+    page.controls.append(
         ft.Row([btn1, btn2, btn3, btnMultiply])
     )
-    page.add(
+    page.controls.append(
         ft.Row([btn0, btnDot, btnDivide, btnEqual])
     )
-    page.add(
-        ft.Row(controls=[
-
-        ])
-    )
-
+    page.update()
 ft.app(main)
