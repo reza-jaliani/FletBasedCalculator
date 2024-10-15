a41f9b3a099b05822d7a8a0c5f44aac4a3a8b472
root <mrz.jaliani@gmail.com>
2024-10-15 17:54:39 +0330

    Add operation functions

--- calculator/main.py
+++ calculator/main.py
@@ -2,30 +2,99 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    num = ""    # This will store the string of current entered numbers
-    number1 = 0 # This will store the first number for operators
-    number2 = 0 # This will store the Second number for operators
+    old_num = 0 # This will store the first number for operators
+    new_num = 0 # This will store the Second number for operators
+    get_num = "" # This will hold the entered number
+    sum_operator = False #This will store the state of sum operator
+    minus_operator = False #This will store the state of minus operator
+    multiply_operator = False # This will store the state of multiply operator
+    divide_operator = False # This will store the state of divide operator
+
+    # Function to reset everything
+    def btnAC_clicked(e):
+        nonlocal get_num, old_num, new_num, sum_operator, minus_operator, multiply_operator, divide_operator
+        t.value = "0"
+        get_num = 0
+        old_num = 0
+        new_num = 0
+        sum_operator = False
+        minus_operator = False
+        multiply_operator = False
+        divide_operator = False
 
     # Function to handle number buttons click
     def btnNum_clicked(e):
-        nonlocal num
-        num += str(e.control.data)
-        t.value = num
+        nonlocal get_num
+        get_num += str(e.control.data)
+        t.value = get_num
         page.update()
 
     # Function to sum two numbers
-    def sum():
-        nonlocal number1, num
-        number1 = int(num)
-        t.value = "000"
-        num=""
+    def btnSum_clicked():
+        nonlocal get_num, old_num, new_num, sum_operator
+        sum_operator = True
+        t.value = "0"
+        old_num += new_num
+        new_num = int(get_num)
+        get_num = ""
+        page.update()
+
+    # Function to minus two numbers
+    def btnMinus_clicked():
+        nonlocal get_num, old_num, new_num, minus_operator
+        minus_operator = True
+        t.value = "0"
+        old_num = new_num - old_num
+        new_num = int(get_num)
+        get_num = ""
         page.update()
     
+    # Function to multiply two numbers
+    def btnMultiply_clicked():
+        nonlocal get_num, old_num, new_num, multiply_operator
+        multiply_operator = True
+        if (old_num == 0): old_num = 1
+        t.value = "0"
+        old_num *= new_num
+        new_num = int(get_num)
+        get_num = ""
+        page.update()
+    
+    # Funvtion to divide two numbers
+    def btnDivide_clicked():
+        nonlocal get_num, old_num, new_num, divide_operator
+        divide_operator = True
+        if (old_num == 0): old_num = new_num
+        new_num = int(get_num)
+        if (new_num != 0): old_num = old_num / new_num
+        t.value = "0"
+        get_num = ""
+        page.update()
+
     # Function to handle the equal button
     def equal():
-        nonlocal number2
-        number2 = number1 + int(num)
-        t.value = str(number2)
+        nonlocal minus_operator, sum_operator, multiply_operator, divide_operator, old_num, new_num
+        if sum_operator:
+            btnSum_clicked()
+            sum_operator = False
+            new_num = new_num + old_num
+            t.value = new_num
+        elif minus_operator:
+            btnMinus_clicked()
+            minus_operator = False
+            new_num = old_num - new_num
+            t.value = new_num
+        elif multiply_operator:
+            btnMultiply_clicked()
+            multiply_operator = False
+            new_num = old_num * new_num
+            t.value = new_num
+        elif divide_operator:
+            btnDivide_clicked()
+            divide_operator = False
+            t.value = old_num
+        else:
+            t.value = "Do something first and then ask me the answer"
         page.update()
 
     t = ft.Text("Hello Flet!", color="Red")
@@ -40,12 +109,12 @@ def main(page: ft.Page):
     btn8 = ft.ElevatedButton(text="8", on_click=lambda e:btnNum_clicked(e), data=8)
     btn9 = ft.ElevatedButton(text="9", on_click=lambda e:btnNum_clicked(e), data=9)
     
-    btnPlus = ft.ElevatedButton(text="+", on_click= lambda e: sum())
-    btnMinus = ft.ElevatedButton(text="-")
-    btnMultiply = ft.ElevatedButton(text="*")
-    btnDot = ft.ElevatedButton(text=".")
+    btnPlus = ft.ElevatedButton(text="+", on_click= lambda e: btnSum_clicked(), data=True)
+    btnMinus = ft.ElevatedButton(text="-", on_click= lambda e: btnMinus_clicked(), data=True)
+    btnMultiply = ft.ElevatedButton(text="*", on_click= lambda e: btnMultiply_clicked(), data=True)
+    btnDot = ft.ElevatedButton(text="/", on_click= lambda e: btnDivide_clicked(), data=True)
     btnEqual = ft.ElevatedButton(text="=", on_click=lambda e: equal())
-    btnDivide = ft.ElevatedButton(text="/")
+    btnDivide = ft.ElevatedButton(text="AC", on_click=lambda e: btnAC_clicked())
     
     
     page.controls.append(ft.SafeArea(t))
